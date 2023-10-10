5dfb5ee847516cf03b6cba6ee4ff51139de1e965
Ed <edwinrosemond1@gmail.com>
2023-10-10 11:40:11 -0400

    new dev branch

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore"; // Import necessary Firestore methods
 
 const config = {
-  apiKey: "AIzaSyAey-qPSicrLoZRBWglMd8N8QmtmiQL8Fg",
-  authDomain: "localhost",
-  projectId: "calendar-buddy-bfdf1",
-  storageBucket: "gs://calendar-buddy-bfdf1.appspot.com",
-  messagingSenderId: "337284482688",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const app = initializeApp(config, {
